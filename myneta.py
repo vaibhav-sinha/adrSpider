1cad22a687c5251da291e3b6820518749564b091
vaibhav-sinha <vaibhavsinh@gmail.com>
2015-02-25 14:21:51 +0530

    Publishing

--- myneta.py
+++ myneta.py
@@ -18,6 +18,8 @@ def scrape(url,filename):
 	neta_list = []
 	email=''
 	phone=''
+
+	print names;
 	
 	for i in range(0,len(sid)):
 		page_u = requests.get(link[2*i+1])
@@ -49,7 +51,7 @@ def scrape(url,filename):
 #scrape('http://www.myneta.info/assam2011/index.php?action=show_winners&sort=default','Assam')
 #scrape('http://www.myneta.info/bih2010/index.php?action=show_winners&sort=default','Bihar')
 #scrape('http://www.myneta.info/chattisgarh2013/index.php?action=show_winners&sort=default','Chattishgarh')
-#scrape('http://www.myneta.info/delhi2013/index.php?action=show_winners&sort=default','Delhi')
+scrape('http://www.myneta.info/delhi2013/index.php?action=show_winners&sort=default','Delhi')
 #scrape('http://www.myneta.info/goa2012/index.php?action=show_winners&sort=default','Goa')
 #scrape('http://www.myneta.info/gujarat2012/index.php?action=show_winners&sort=default','Gujarat')
 #scrape('http://www.myneta.info/ha2009/index.php?action=show_winners&sort=default','Haryana')
@@ -58,19 +60,19 @@ def scrape(url,filename):
 #scrape('http://www.myneta.info/jarka09/index.php?action=show_winners&sort=default','Jharkhand')
 #scrape('http://www.myneta.info/karnataka2013/index.php?action=show_winners&sort=default','Karnataka')
 #scrape('http://www.myneta.info/kerala2011/index.php?action=show_winners&sort=default','Kerala')
-scrape('http://www.myneta.info/mp2013/index.php?action=show_winners&sort=default','MadhyaPradesh')
-scrape('http://www.myneta.info/mh2009/index.php?action=show_winners&sort=default','Maharashtra')
-scrape('http://www.myneta.info/manipur2012/index.php?action=show_winners&sort=default','Manipur')
-scrape('http://www.myneta.info/meghalaya2013/index.php?action=show_winners&sort=default','Meghalaya')
-scrape('http://www.myneta.info/mizoram2013/index.php?action=show_winners&sort=default','Mizoram')
-scrape('http://www.myneta.info/nagaland2013/index.php?action=show_winners&sort=default','Nagaland')
-scrape('http://www.myneta.info/odisha2014/index.php?action=show_winners&sort=default','Odisha')
-scrape('http://www.myneta.info/puducherry2011/index.php?action=show_winners&sort=default','Puducherry')
-scrape('http://www.myneta.info/pb2012/index.php?action=show_winners&sort=default','Punjab')
-scrape('http://www.myneta.info/rajasthan2013/index.php?action=show_winners&sort=default','Rajasthan')
-scrape('http://www.myneta.info/sikkim2014/index.php?action=show_winners&sort=default','Sikkim')
-scrape('http://www.myneta.info/tamilnadu2011/index.php?action=show_winners&sort=default','TamilNadu')
-scrape('http://www.myneta.info/tripura2013/index.php?action=show_winners&sort=default','Tripura')
-scrape('http://www.myneta.info/utt2012/index.php?action=show_winners&sort=default','Uttarakhand')
-scrape('http://www.myneta.info/up2012/index.php?action=show_winners&sort=default','UttarPradesh')
-scrape('http://www.myneta.info/westbengal2011/index.php?action=show_winners&sort=default','WestBengal')
+#scrape('http://www.myneta.info/mp2013/index.php?action=show_winners&sort=default','MadhyaPradesh')
+#scrape('http://www.myneta.info/mh2009/index.php?action=show_winners&sort=default','Maharashtra')
+#scrape('http://www.myneta.info/manipur2012/index.php?action=show_winners&sort=default','Manipur')
+#scrape('http://www.myneta.info/meghalaya2013/index.php?action=show_winners&sort=default','Meghalaya')
+#scrape('http://www.myneta.info/mizoram2013/index.php?action=show_winners&sort=default','Mizoram')
+#scrape('http://www.myneta.info/nagaland2013/index.php?action=show_winners&sort=default','Nagaland')
+#scrape('http://www.myneta.info/odisha2014/index.php?action=show_winners&sort=default','Odisha')
+#scrape('http://www.myneta.info/puducherry2011/index.php?action=show_winners&sort=default','Puducherry')
+#scrape('http://www.myneta.info/pb2012/index.php?action=show_winners&sort=default','Punjab')
+#scrape('http://www.myneta.info/rajasthan2013/index.php?action=show_winners&sort=default','Rajasthan')
+#scrape('http://www.myneta.info/sikkim2014/index.php?action=show_winners&sort=default','Sikkim')
+#scrape('http://www.myneta.info/tamilnadu2011/index.php?action=show_winners&sort=default','TamilNadu')
+#scrape('http://www.myneta.info/tripura2013/index.php?action=show_winners&sort=default','Tripura')
+#scrape('http://www.myneta.info/utt2012/index.php?action=show_winners&sort=default','Uttarakhand')
+#scrape('http://www.myneta.info/up2012/index.php?action=show_winners&sort=default','UttarPradesh')
+#scrape('http://www.myneta.info/westbengal2011/index.php?action=show_winners&sort=default','WestBengal')
